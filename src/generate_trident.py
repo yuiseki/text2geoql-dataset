2fd42d5b0116ccdf87d9b7ab1693075c2ffc584f
yuiseki <yuiseki@gmail.com>
2024-05-03 15:48:38 +0900

    Fix mistake

--- src/generate_trident.py
+++ src/generate_trident.py
@@ -1,21 +1,8 @@
 import hashlib
 import os
 import sys
-import ollama
-from langchain_chroma import Chroma
-from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 
-embeddings = OllamaEmbeddings(
-    model="all-minilm:l6-v2",
-)
-vectorstore = Chroma("langchain_store", embeddings)
-
-example_selector = SemanticSimilarityExampleSelector(
-    vectorstore=vectorstore,
-    k=10,
-)
+examples = []
 
 
 def add_examples_from_dir(directory):
@@ -23,57 +10,15 @@ def add_examples_from_dir(directory):
         for file in files:
             if file == "input-trident.txt":
                 input_txt = open(os.path.join(root, file), "r").read().strip()
-                if input_txt.startswith("Area:"):
-                    example = {
-                        "input": input_txt,
-                    }
-                    example_selector.add_example(example)
+                example = {
+                    "input": input_txt,
+                    "path": os.path.join(root, file),
+                }
+                examples.append(example)
 
 
 dir_path = "./data"
 add_examples_from_dir(dir_path)
 
-example_prompt = PromptTemplate(
-    input_variables=["input"],
-    template="Output:\n{input}",
-)
-
-prompt_prefix = """\
-You output list of domain-specific language to retrieve information about requested area.
-
-You will always reply according to the following rules:
-- `Area:` is the area specifier.
-- `AreaWithConcern:` is the concern specifier with specific area.
-- All output MUST be start with specifier above.
-- Output MUST be multi-line, markdown formatted list.
-- Expand similar DSLs based on your knowledge.
-- MUST NOT repeat the examples.
-
-===
-Examples:\
-"""
-
-dynamic_prompt = FewShotPromptTemplate(
-    example_selector=example_selector,
-    example_prompt=example_prompt,
-    prefix=prompt_prefix,
-    suffix="===\nRequested Area:\n{instruction}\n\nOutput:",
-    input_variables=["instruction"],
-)
-
-instruct = sys.argv[1]
-prompt = dynamic_prompt.format(instruction=instruct)
-
-print(prompt)
-
-response = ollama.generate(
-    prompt=prompt,
-    model='tinydolphin:1.1b',
-    # model='tinyllama:1.1b',
-    # model='phi3:3.8b',
-    options={
-        'temperature': 0.01,
-        'num_predict': 128,
-    })
-
-print(response['response'])
+for example in examples:
+    print(example)
