acc1f5bddf5d4c144e7578b099552d0822ae4a8a
yuiseki <yuiseki@gmail.com>
2024-05-05 08:45:27 +0900

    Update src/find_orphan_trident.py

--- src/find_orphan_trident.py
+++ src/find_orphan_trident.py
@@ -1,13 +1,15 @@
 import os
 import sys
-# 引数で与えられたディレクトリ内で input-trident.txt だけがあって output-*.overpassql がないパスを探して出力する
+# 引数で与えられたディレクトリ内で input-trident.txt だけがあって not-found.txt も output-*.overpassql もないパスを探して出力する
 
 dir_path = sys.argv[1]
 
 
 def find_orphan_trident(dir_path):
     for root, dirs, files in os.walk(dir_path):
-        if "input-trident.txt" in files and not any(f.startswith("output-") and f.endswith(".overpassql") for f in files):
+        if "input-trident.txt" in files and not "not-found.txt" in files and not any(
+            f.startswith("output-") and f.endswith(".overpassql") for f in files
+        ):
             print(root)
 
 
